63fb21762e6982e09892511ede7c61db5e143db1
Kevin Newman <CaptainN@unFocus.com>
2012-07-09 20:26:46 -0400

    Made dispatcher go through a DOM event. Fixes #2.

--- src/SignalLite.js
+++ src/SignalLite.js
@@ -1,5 +1,11 @@
 ﻿(function() { "use strict";
 
+var SIGNAL_EVENT = "signalLiteEvent";
+
+var ua = navigator.userAgent;
+var isFirefox = ua.indexOf( "compatible" ) < 0 &&
+	/Mozilla/.test( ua );
+
 /**
  * Holds the listener to be called by the Signal (and provides properties for a simple linked list).
  * @author Kevin Newman
@@ -132,16 +138,75 @@ SignalLite.prototype = {
 	},
 	
 	/**
-	 * Dispatches an event.
+	 * Dispatches an event. Uses Dean Edwards DOM dispatching to avoid 
+	 * blocking dispatch on error, and to avoid suppressing those errors.
+	 * @link http://dean.edwards.name/weblog/2009/03/callbacks-vs-events/
 	 */
 	dispatch: function dispatch()
 	{
-		var node = this.first;
-		while ( node = node.next ) {
-			node.listener.apply( node.target || this.target, arguments );
+		function getSignalClosure( signal, node, args ) {
+			return function listener() {
+				document.removeEventListener( SIGNAL_EVENT, listener, false );
+				try {
+					node.listener.apply( node.target || signal.target, args );
+				}
+				catch ( e ) {
+					// Firefox is supporessing this for some reason, so we'll 
+					// manually report the error, until I figure out why.
+					if ( isFirefox && console )
+						console.error( e );
+					throw e;
+				}
+			};
 		}
+		
+		var se = document.createEvent( "UIEvents" );
+		se.initEvent( SIGNAL_EVENT, false, false );
+		
+		var node = this.first;
+		
+		// Building this dispatch list essentially copies the dispatch list, so 
+		// add/removes during dispatch won't have any effect. BONUS~!
+		while ( node = node.next ) {
+			document.addEventListener( SIGNAL_EVENT,
+				getSignalClosure( this, node, arguments ), false
+			);
+		}
+		
+		document.dispatchEvent( se );
 	}
 };
+
+// IE 8 and lower
+if ( !document.addEventListener )
+{
+	document.documentElement[ SIGNAL_EVENT ] = 0;
+	
+	getSignalClosure = function( signal, node, args ) {
+		return function listener( event )
+		{
+			if (event.propertyName == SIGNAL_EVENT) {
+				document.documentElement.detachEvent( "onpropertychange",
+					 // using named inline ref (listener) didn't work here...
+					arguments.callee, false
+				);
+				node.listener.apply( node.target || signal.target, args );
+			}
+		};
+	};
+	SignalLite.prototype.dispatch = function dispatch()
+	{
+		var node = this.first;
+		while ( node = node.next ) {
+			document.documentElement.attachEvent( "onpropertychange",
+				getSignalClosure( this, node, arguments ), false
+			);
+		}
+		
+		// triggers the property change event
+		++document.documentElement[ SIGNAL_EVENT ];
+	};
+}
 
 // This has to be assigned here, rather than inline because of bugs in IE7/IE8
 window.SignalLite = SignalLite;
