8adfec52d7d150bd1bc5e51dfb069f7de8037def
Kevin Newman <CaptainN@unFocus.com>
2012-11-21 00:55:27 -0500

    That interface idea didn't work out...

--- src/SignalsLite.d.ts
+++ src/SignalsLite.d.ts
@@ -1,4 +1,4 @@
-interface SignalLite {
+declare class SignalLite {
 	constructor( target?: any, eachReturn?: ( ...args: any[] ) => any );
 	add( listener: ( ...args: any[] ) => any, target?: any ) : void;
 	addToTop( listener: ( ...args: any[] ) => any, target?: any ) : void;
@@ -10,31 +10,32 @@ interface SignalLite {
 	stopDispatch() : void;
 	eachReturn: ( ...args: any[] ) => any;
 }
-interface SignalNamespace {
+declare class SignalNamespace {
 	add( listener: ( ...args: any[] ) => any, target?: any ): void;
 	addToTop( listener: ( ...args: any[] ) => any, target?: any ): void;
 	once( listener: (...args: any[] ) => any, target?: any ): void;
 	remove( listener: (...args: any[] ) => any ): void;
 	removeAll():void;
 }
-interface SignalNamespaceMgr {
+declare class SignalNamespaceMgr {
 	add( name: string );
 	remove( name: string );
 }
-interface SignalPriority {
+declare class SignalPriority {
 	add( listener: (...args: any[] ) => any, target?: any );
 	addToTop( listener: (...args: any[] ) => any, target?: any );
 	once( listener: (...args: any[] ) => any, target?: any );
 }
+// :TODO: ns has .add and .remove nodes, figure out how to specify that in TS.
 // :TODO: ns.add adds a property to the signal, figure out how to allow in TS.
-interface Signal extends SignalLite {
-	ns: SignalNamespaceMgr;
+declare class Signal extends SignalLite {
+	//ns: SignalNamespaceMgr;
 	ns( name: string ): SignalNamespace;
-	namespace: SignalNamespaceMgr;
+	//namespace: SignalNamespaceMgr;
 	namespace( name: string ): SignalNamespace;
 	priority( priority: number ): SignalPriority;
 }
-interface SignalDispatcher extends Signal {
+declare class SignalDispatcher extends Signal {
 	constructor( target?: any, eachReturn?: ( any? ) => any, eachError?: ( any? ) => any );
 	dispatch( ...args: any[] ) : void;
 	eachError: ( ...args: any[] ) => any;
