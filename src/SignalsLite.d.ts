124deea2a393de41d21a121f0e870280b2ea3e2e
Kevin Newman <CaptainN@unFocus.com>
2012-11-21 00:34:14 -0500

    Added a TypeScript declaration file.
    This hasn't been tested in any vigorous way, but it does compile.

---
+++ src/SignalsLite.d.ts
@@ -0,0 +1,42 @@
+declare class SignalLite {
+	constructor( target?: any, eachReturn?: ( any? ) => any );
+	add( listener: ( ...args: any[] ) => any, target?: any ) : void;
+	addToTop( listener: () => any, target?: any ) : void;
+	once( listener: () => any, target?: any ) : void;
+	remove( listener: () => any ) : void;
+	removeAll() : void;
+	getLength() : number;
+	trigger( ...args: any[] ) : void;
+	stopDispatch() : void;
+	eachReturn: ( ...args: any[] ) => any;
+}
+declare class SignalNamespace {
+	add( listener: ( ...args: any[] ) => any, target?: any ): void;
+	addToTop( listener: ( ...args: any[] ) => any, target?: any ): void;
+	once( listener: (...args: any[] ) => any, target?: any ): void;
+	remove( listener: (...args: any[] ) => any ): void;
+	removeAll():void;
+}
+declare class SignalNamespaceMgr {
+	add( name: string );
+	remove( name: string );
+}
+declare class SignalPriority {
+	add( listener: (...args: any[] ) => any, target?: any );
+	addToTop( listener: (...args: any[] ) => any, target?: any );
+	once( listener: (...args: any[] ) => any, target?: any );
+}
+// :TODO: ns has .add and .remove nodes, figure out how to specify that in TS.
+// :TODO: ns.add adds a property to the signal, figure out how to allow in TS.
+declare class Signal extends SignalLite {
+	//ns: SignalNamespaceMgr;
+	ns( name: string ): SignalNamespace;
+	//namespace: SignalNamespaceMgr;
+	namespace( name: string ): SignalNamespace;
+	priority( priority: number ): SignalPriority;
+}
+declare class SignalDispatcher extends Signal {
+	constructor( target?: any, eachReturn?: ( any? ) => any, eachError?: ( any? ) => any );
+	dispatch( ...args: any[] ) : void;
+	eachError: ( ...args: any[] ) => any;
+}
